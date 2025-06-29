c655e6b60df1811c5222b8b151c10e28b8235fec
KevinValladares06 <149841068+KevinValladares06@users.noreply.github.com>
2025-06-28 18:52:30 -0600

    Actualizacion clase

--- src/Dao/Products/Products.php
+++ src/Dao/Products/Products.php
@@ -12,32 +12,6 @@ class Products extends Table
         $params = [];
         $registros = self::obtenerRegistros($sqlstr, $params);
         return $registros;
-        /*return [
-
-            [
-                "productId" => 1,
-                "productName" => "Producto 1",
-                "productDescription" => "Descripción del producto 1",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productUnitPrice" => 100.00
-            ],
-            [
-                "productId" => 2,
-                "productName" => "Producto 2",
-                "productDescription" => "Descripción del producto 2",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productUnitPrice" => 120.00
-            ],
-
-            [
-                "productId" => 3,
-                "productName" => "Producto 3",
-                "productDescription" => "Descripción del producto 3",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productUnitPrice" => 70.00
-            ],
-
-        ];*/
     }
 
     public static function getNewProducts()
@@ -46,22 +20,6 @@ class Products extends Table
         $params = [];
         $registros = self::obtenerRegistros($sqlstr, $params);
         return $registros;
-        /*return [
-            [
-                "productId" => 99,
-                "productName" => "Producto 99",
-                "ProductoDescription" => "Descripción del producto Nuevo 99",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productPrice" => 50.00
-            ],
-            [
-                "productId" => 100,
-                "productName" => "Producto 100",
-                "ProductoDescription" => "Descripción del producto Nuevo 100",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productPrice" => 130.00
-            ]
-        ];*/
     }
 
     public static function getDailyDeals()
@@ -70,29 +28,5 @@ class Products extends Table
         $params = [];
         $registros = self::obtenerRegistros($sqlstr, $params);
         return $registros;
-        /*return [
-            [
-                "productId" => 73,
-                "productName" => "Producto 73",
-                "productDescription" => "Descripción del producto 73",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productPrice" => 10.00
-            ],
-            [
-                "productId" => 15,
-                "productName" => "Producto 15",
-                "productDescription" => "Descripción del producto 15",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productPrice" => 13.00
-            ],
-            [
-                "productId" => 10,
-                "productName" => "Producto 10",
-                "productDescription" => "Descripción del producto 10",
-                "productImgUrl" => "https://via.placeholder.com/150",
-                "productPrice" => 20.00
-            ]
-
-        ];*/
     }
 }
